f6a5acf46c3fd6d9a3e52532fea4c9b95c0f84c0
Rob Lee <rob@spartansystems.co>
2015-06-03 13:43:44 -0400

    readycart workers

--- deploy/recipes/readycart-workers.rb
+++ deploy/recipes/readycart-workers.rb
@@ -5,6 +5,13 @@
 
 include_recipe 'deploy'
 
+if node["opsworks"].has_key?("instance") && node["opsworks"]["instance"].has_key?("layers") && node["opsworks"]["instance"]["layers"].include?("readycart_app")
+  execute "change www-data home directory" do
+    command "if echo ~www-data | grep www-data > /dev/null; then echo 0; else sudo usermod -d /home/www-data www-data > /dev/null; fi"
+    action :run
+  end
+end
+
 node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'other'
     Chef::Log.debug("Skipping deploy::php application #{application} as it is not an Other app")
