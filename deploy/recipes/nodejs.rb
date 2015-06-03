55cf8504d4af93fe99e2a8b13f6f3a01dfe58528
Rob Lee <rob@spartansystems.co>
2015-06-03 12:39:21 -0400

    nodejs

--- deploy/recipes/nodejs.rb
+++ deploy/recipes/nodejs.rb
@@ -7,7 +7,7 @@ node[:deploy].each do |application, deploy|
   end
 
   execute "change www-data home directory" do
-    command "id -u www-data || useradd -d /home/www-data -m -U www-data"
+    command "id -u www-data || useradd -d /home/www-data -s /bin/bash -m -U www-data"
     action :run
   end
 
