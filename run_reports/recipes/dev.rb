dae2b6020450e544d5a286c8cbb4a75dd5aedf0e
Rob Lee <rob@spartansystems.co>
2015-06-10 01:02:21 -0400

    dev

---
+++ run_reports/recipes/dev.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: run_reports
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "run reports" do
+  command "cd /srv/www/readycart_workers/current && php run cron --env=dev && php run reports --env=dev"
+  action :run
+end
