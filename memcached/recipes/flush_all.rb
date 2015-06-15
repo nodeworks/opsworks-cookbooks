bd1f583e3ec326cdca0ad6808d46be90d811d6df
Rob Lee <rob@spartansystems.co>
2015-06-15 11:03:41 -0400

    flush all memcached

--- memcached/recipes/flush_all.rb
+++ memcached/recipes/flush_all.rb
@@ -1,8 +1,7 @@
-if node["opsworks"].has_key?("instance") && node["opsworks"]["instance"].has_key?("layers") && node["opsworks"]["instance"]["layers"].include?("memcached")
-  node[:opsworks][:layers]['memcached'][:instances].each do |instance_name, instance|
-    execute "create public/private files directory" do
-      command "nc #{instance['ip']} 11211 <<< 'flush_all'"
-      action :run
-    end
+node[:opsworks][:layers][:memcached][:instances].each do |instance_name, instance|
+  Chef::Log.debug("Flushing #{instance_name}")
+  execute "flush all memcached instances" do
+    command "nc #{instance['ip']} 11211 <<< 'flush_all'"
+    action :run
   end
 end
