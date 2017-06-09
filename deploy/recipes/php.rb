f62ed4da9b2f9b7351ecd630f46c87815ebbc1f2
Rob Lee <rob@nodeworks.com>
2017-06-08 20:49:46 -0400

    php update

--- deploy/recipes/php.rb
+++ deploy/recipes/php.rb
@@ -6,8 +6,8 @@
 include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
-  if deploy[:application_type] != 'other'
-    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an Other app")
+  if deploy[:application_type] != 'php'
+    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
     next
   end
 
